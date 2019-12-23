140962ffb50f7777d3eb5c42ab50dca567939800
Juakata <ha.uzquiano@hotmail.com>
2019-12-23 14:09:05 -0600

    Improve the way i call promises and handle the errors.

--- src/index.js
+++ src/index.js
@@ -5,28 +5,25 @@ if ('geolocation' in navigator) {
     const { latitude, longitude } = position.coords;
 
     const proxy = 'https://cors-anywhere.herokuapp.com/';
-    fetch(`${proxy}https://api.darksky.net/forecast/c585ab3790ed34a967b7c3810abd299b/${latitude},${longitude}`)
-      .then(response => response.json())
-      .then((response) => {
-        const { timezone, currently } = response;
-        const { temperature, summary, humidity } = currently;
-
-        document.getElementById('timezone').innerHTML = timezone;
-        document.getElementById('temperature').innerHTML = temperature;
-        document.getElementById('summary').innerHTML = summary;
-        document.getElementById('humidity').innerHTML = humidity;
-        fetch(`https://api.giphy.com/v1/gifs/translate?api_key=11Fe1ivk6MxFFhuVm6lZdo7ZRtLwQ1Kf&s=${summary}-cloudy`)
-          .then(response => response.json())
-          .then((response) => {
-            document.getElementById('img-weather').src = response.data.images.original.url;
-          })
-          .catch((error) => {
-            document.getElementById('error').innerHTML = error;
-          });
-      })
-      .catch((error) => {
+    Promise.all([
+      fetch(`${proxy}https://api.darksky.net/forecast/c585ab3790ed34a967b7c3810abd299b/${latitude},${longitude}`)
+        .then(value => value.json()),
+      fetch(`https://api.giphy.com/v1/gifs/translate?api_key=11Fe1ivk6MxFFhuVm6lZdo7ZRtLwQ1Kf&s=${summary}-cloudy`)
+        .then(value => value.json())
+    ]).then((responses) => {
+      const response1 = responses[0];
+      const response2 = responses[1];
+      const { timezone, currently } = response1;
+      const { temperature, summary, humidity } = currently;
+      document.getElementById('timezone').innerHTML = timezone;
+      document.getElementById('temperature').innerHTML = temperature;
+      document.getElementById('summary').innerHTML = summary;
+      document.getElementById('humidity').innerHTML = humidity;
+      document.getElementById('img-weather').src = response2.data.images.original.url;
+    }).catch((error) => {
+        document.getElementById('error').style.display = 'block';
         document.getElementById('error').innerHTML = error;
-      });
+    });
   });
 } else {
   document.getElementById('error').innerHTML = error;
