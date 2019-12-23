b414e5ef0d89cb7bded46a4f4fe051bdd7934dfa
Juakata <ha.uzquiano@hotmail.com>
2019-12-23 12:56:12 -0600

    Add variables to global, fix object destructuring.

--- src/index.js
+++ src/index.js
@@ -2,27 +2,21 @@ import './style.css';
 
 if ('geolocation' in navigator) {
   navigator.geolocation.getCurrentPosition((position) => {
-    const latitude = position.coords.latitude;
-    const longitude = position.coords.longitude;
+    const { latitude, longitude } = position.coords;
+
     const proxy = 'https://cors-anywhere.herokuapp.com/';
     fetch(`${proxy}https://api.darksky.net/forecast/c585ab3790ed34a967b7c3810abd299b/${latitude},${longitude}`)
-      .then((response) => {
-        return response.json();
-      })
+      .then(response => { return response.json(); })
       .then((response) => {
         const timezone = response.timezone;
-        const temperature = response.currently.temperature;
-        const forImg = response.currently.icon;
-        const summary = response.currently.summary;
-        const humidity = response.currently.humidity;
+        const { temperature, summary, humidity } = response.currently
+
         document.getElementById('timezone').innerHTML = timezone;
         document.getElementById('temperature').innerHTML = temperature;
         document.getElementById('summary').innerHTML = summary;
         document.getElementById('humidity').innerHTML = humidity;
-        fetch(`https://api.giphy.com/v1/gifs/translate?api_key=11Fe1ivk6MxFFhuVm6lZdo7ZRtLwQ1Kf&s=${forImg}-weather`)
-          .then((response) => {
-            return response.json();
-          })
+        fetch(`https://api.giphy.com/v1/gifs/translate?api_key=11Fe1ivk6MxFFhuVm6lZdo7ZRtLwQ1Kf&s=${summary}-cloudy`)
+          .then(response => { return response.json(); })
           .then((response) => {
             document.getElementById("img-weather").src = response.data.images.original.url;
           })
