88dc0bbcbc0c9a82c715e3fcdac753221c4cc743
Juakata <ha.uzquiano@hotmail.com>
2019-12-24 12:32:18 -0600

    Add a form to search for a spesific city

--- src/index.js
+++ src/index.js
@@ -1,35 +1,67 @@
 import './style.css';
 
-if ('geolocation' in navigator) {
-  navigator.geolocation.getCurrentPosition((position) => {
-    const { latitude, longitude } = position.coords;
+const proxy = 'https://cors-anywhere.herokuapp.com/';
 
-    const proxy = 'https://cors-anywhere.herokuapp.com/';
-    fetch(`${proxy}https://api.darksky.net/forecast/c585ab3790ed34a967b7c3810abd299b/${latitude},${longitude}`)
+const setGift = (summary) => {
+  fetch(`https://api.giphy.com/v1/gifs/translate?api_key=11Fe1ivk6MxFFhuVm6lZdo7ZRtLwQ1Kf&s=${summary}%20cloudy`)
+    .then(response => response.json())
+    .then((response) => {
+      document.getElementById('img-weather').src = response.data.images.original.url;
+    })
+    .catch((error) => {
+      document.getElementById('error').style.display = 'block';
+      document.getElementById('error').innerHTML = error;
+    });
+}
+
+const getWeatherInfo = (coords) => {
+  fetch(`${proxy}https://api.darksky.net/forecast/c585ab3790ed34a967b7c3810abd299b/${coords[0]},${coords[1]}`)
+    .then(response => response.json())
+    .then((response) => {
+      const { timezone, currently } = response;
+      const { temperature, summary, humidity } = currently;
+
+      document.getElementById('timezone').innerHTML = timezone;
+      document.getElementById('temperature').innerHTML = temperature;
+      document.getElementById('summary').innerHTML = summary;
+      document.getElementById('humidity').innerHTML = humidity;
+
+      setGift(summary);
+    })
+    .catch((error) => {
+      document.getElementById('error').style.display = 'block';
+      document.getElementById('error').innerHTML = error;
+    });
+}
+
+const startGeolocation = (city) => {
+  document.getElementById('error').style.display = 'none';
+  if (('geolocation' in navigator) && city === '') {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const coords = [];
+      coords.push(position.coords.latitude);
+      coords.push(position.coords.longitude);
+      getWeatherInfo(coords);
+    });
+  } else {
+    fetch(`${proxy}http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=256d86b132c962dc71862d23501ecdc9`)
       .then(response => response.json())
       .then((response) => {
-        const { timezone, currently } = response;
-        const { temperature, summary, humidity } = currently;
-
-        document.getElementById('timezone').innerHTML = timezone;
-        document.getElementById('temperature').innerHTML = temperature;
-        document.getElementById('summary').innerHTML = summary;
-        document.getElementById('humidity').innerHTML = humidity;
-        fetch(`https://api.giphy.com/v1/gifs/translate?api_key=11Fe1ivk6MxFFhuVm6lZdo7ZRtLwQ1Kf&s=${summary}-cloudy`)
-          .then(response => response.json())
-          .then((response) => {
-            document.getElementById('img-weather').src = response.data.images.original.url;
-          })
-          .catch((error) => {
-            document.getElementById('error').style.display = 'block';
-            document.getElementById('error').innerHTML = error;
-          });
+        const coords = [];
+        coords.push(response.coord.lat);
+        coords.push(response.coord.lon);
+        getWeatherInfo(coords);
       })
       .catch((error) => {
         document.getElementById('error').style.display = 'block';
-        document.getElementById('error').innerHTML = error;
+        document.getElementById('error').innerHTML = 'Something went wrong try again';
       });
-  });
-} else {
-  document.getElementById('error').innerHTML = error;
+  }
 }
+
+startGeolocation('');
+
+document.getElementById('send').addEventListener('click', () => {
+  const city = document.getElementById('city').value;
+  startGeolocation(city);
+}, false);
